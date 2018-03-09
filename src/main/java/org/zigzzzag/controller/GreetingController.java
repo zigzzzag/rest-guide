4185141bc484a4e44b776ea40e46307f58402e7b
zigzzzag <maximnikiforov1991@gmail.com>
2018-03-09 16:26:13 +0700

    add swagger and log4j

--- src/main/java/org/zigzzzag/controller/GreetingController.java
+++ src/main/java/org/zigzzzag/controller/GreetingController.java
@@ -1,6 +1,7 @@
 package org.zigzzzag.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.zigzzzag.model.Greeting;
@@ -13,7 +14,7 @@ public class GreetingController {
     private static final String TEMPLATE = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
     public Greeting greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
     }
