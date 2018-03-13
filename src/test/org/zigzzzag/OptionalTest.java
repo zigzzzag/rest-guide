425a277f6de31a0a8864ecd3a790a178c457db3c
zigzzzag <maximnikiforov1991@gmail.com>
2018-03-14 00:14:19 +0700

    add optional test

--- src/test/org/zigzzzag/OptionalTest.java
+++ src/test/org/zigzzzag/OptionalTest.java
@@ -2,9 +2,12 @@ package org.zigzzzag;
 
 import org.junit.Test;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 public class OptionalTest {
@@ -21,7 +24,6 @@ public class OptionalTest {
         assertTrue(op.isPresent());
     }
 
-    @SuppressWarnings("unusedLocal")
     @Test(expected = NullPointerException.class)
     public void optionalOfTestNPE() {
         Optional.of(null);
@@ -34,5 +36,31 @@ public class OptionalTest {
 
         op = Optional.ofNullable(null);
         assertFalse(op.isPresent());
+    }
+
+    @Test
+    public void optionalGetTest() {
+        Optional<String> op = Optional.ofNullable("zigzzzag");
+        assertEquals("zigzzzag", op.get());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void optionalNullGetTest() {
+        Optional<String> op = Optional.ofNullable(null);
+        assertNull(op.get());
+    }
+
+    @Test
+    public void optionalIfPresentTest() {
+        Optional<String> op = Optional.ofNullable("zigzzzag");
+        final String[] res = {null};
+        op.ifPresent(x -> res[0] = x + x);
+        assertEquals("zigzzzagzigzzzag", res[0]);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void optionalNullIfPresentTest() {
+        Optional<String> op = Optional.ofNullable("zigzzzag");
+        op.ifPresent(null);
     }
 }
